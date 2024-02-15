3664e4711f68e2de3a450ee4012ad971bf48fda7
Adeel Malik <amalik@datical.com>
2024-02-15 16:45:15 -0600

    changed sales to sales_old

---
+++ DBScripts/01_sales_old.sql
@@ -0,0 +1,8 @@
+create table sales_old (
+    id int,
+    first_name varchar (50),
+    last_name varchar (50)
+);
+
+-- GRANT ALL PRIVILEGES ON sales_old TO postgres;
+-- rollback drop table sales_old
